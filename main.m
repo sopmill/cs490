0c846d27035b7a07a8b90ec65c75973b19818ed5
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-14 18:27:18 -0500

    Slice size specification

--- main.m
+++ main.m
@@ -1,9 +1,15 @@
 a = imread("img_40.jpg");
 
 % Standard Deviation affects how noisy the resulting image is
-desiredstd = 32;
+desiredstd = 64;
+
 % Mean affects how bright the resulting image is
 desiredmu = 32;
+
+% Slice size is the side length of the square of pixels 
+% used to calculate the median
+% Must be odd otherwise there's no middle pixel to change
+sliceSize = 5;
 
 % Add gaussian noise
 [p] = gaussian(a, desiredstd, desiredmu);
@@ -14,7 +20,7 @@ title("Original Image");
 subplot(1,3,2), imshow(p,[]);
 title("Noisy Image");
 
-[m] = Median_Filter(p, 5);
+[m] = Median_Filter(p, sliceSize);
 
 subplot(1,3,3), imshow(m,[]);
 title("Median Filtered Image");
