13c741b157889308622164f572ac46e1866286eb
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-14 17:09:00 -0500

    median filter first iteration
    Using https://www.mathworks.com/matlabcentral/answers/36182-how-to-do-median-filter-without-using-medfilt2

--- Median_Filter.m
+++ Median_Filter.m
@@ -1,8 +1,17 @@
 % Group 255
 % Project: Image Denoising Through Midpoint Filter and Scaling 
 
-function [r] = Median_Filter(img,inputArg2)
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [r] = Median_Filter(img, slice)
+    [row, col] = size(img);
+    fimg = zeros(row, col);
+    for i = 2:row-1
+        for j = 2:col-1
+            flt=[img(i-1,j-1),img(i-1,j),img(i-1,j+1),img(i,j-1),img(i,j),img(i,j+1),img(i+1,j-1),img(i+1,j),img(i+1,j+1)];
+            fimg(i,j)=median(flt);
+        end
+    end
+
+    r = fimg;
+
 end
 
