a3275ae0d138c0d8fad48cd2d8698e53139765bd
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-14 12:24:29 -0500

    Update gaussian.m

--- gaussian.m
+++ gaussian.m
@@ -1,10 +1,27 @@
 % Team 255
 % CS 490 DIP
-% Spatial Gaussian Filter
+% Spatial Gaussian Noise
 
-function [g] = gaussian(p, s)
-  % p - input image
-  % s - sigma
+function [g] = gaussian(a, std, mean)
+  % a - input image
+  % std - sigma
+  % mean - mean value
   % g - output image
-  
+
+  % n - noise matrix
+  n = zeros(size(a));
+  p = pi;
+
+  for row=1:size(n, 1)
+    for col=1:size(n,2)
+        exponent = double((((a(row, col) - mean).^2) / ((2*std).^2)) * -1);
+        e = exp(exponent);
+        fraction = (1 / sqrt(2 * p));
+
+        n(row, col) = fraction * e;
+    end
+  end
+
+  g = a + n;
+
 end
