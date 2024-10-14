fd5bc539aacd8373c37db9b8e7fe2f85cbeda699
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-14 13:51:45 -0500

    extreme optimization

--- gaussian.m
+++ gaussian.m
@@ -8,24 +8,7 @@ function [g] = gaussian(a, std, mu)
   % mu - mean value
   % g - output image
 
-  % n - noise matrix
   n = zeros(size(a));
-  p = pi;
-
-  for row=1:size(n, 1)
-    for col=1:size(n,2)
-        expNumerator = (2 .* (std.^2));
-        expDenominator = (double(a(row, col)) - mu).^2;
-        exponent = (expNumerator ./ expDenominator) .* -1;
-        e = exp(exponent);
-        fraction = (1 / (std * sqrt(2 * p)));
-
-        n(row, col) = fraction .* e;
-    end
-  end
-
-  g(:) = a(:) + (sqrt(std.^2)*randn(size(a)) + mu);
-  %g = (double(a) + n);
-  
-
+  n = double(a) + (std .* randn(size(a)) + mu);
+  g = uint8(n);
 end
