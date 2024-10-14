9081a6bc3a94bc33e25e4cc76b720d2f5a2b4a51
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-14 13:52:03 -0500

    Comments

--- main.m
+++ main.m
@@ -1,11 +1,15 @@
 a = imread("img_40.jpg");
-desiredstd = 50;
-desiredmu = 256;
 
+% Standard Deviation affects how noisy the resulting image is
+desiredstd = 32;
+% Mean affects how bright the resulting image is
+desiredmu = 32;
+
+% Add gaussian noise
 [p] = gaussian(a, desiredstd, desiredmu);
 
-subplot(2,3,1), imshow(a,[]);
+subplot(1,2,1), imshow(a,[]);
 title("Original Image");
 
-subplot(2,3,2), imshow(p,[]);
+subplot(1,2,2), imshow(p,[]);
 title("Noisy Image");
