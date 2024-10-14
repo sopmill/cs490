e48acb4959322d24747eb323533f149c640ef21b
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-14 17:09:14 -0500

    median filter subplot

--- main.m
+++ main.m
@@ -8,8 +8,13 @@ desiredmu = 32;
 % Add gaussian noise
 [p] = gaussian(a, desiredstd, desiredmu);
 
-subplot(1,2,1), imshow(a,[]);
+subplot(1,3,1), imshow(a,[]);
 title("Original Image");
 
-subplot(1,2,2), imshow(p,[]);
+subplot(1,3,2), imshow(p,[]);
 title("Noisy Image");
+
+[m] = Median_Filter(p, 5);
+
+subplot(1,3,3), imshow(m,[]);
+title("Median Filtered Image");
