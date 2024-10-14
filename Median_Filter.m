9d5cb81f64b476b14bcca7362168578489073fc2
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-14 17:13:08 -0500

    header, comments

--- Median_Filter.m
+++ Median_Filter.m
@@ -1,9 +1,12 @@
-% Group 255
-% Project: Image Denoising Through Midpoint Filter and Scaling 
+% Team 255
+% CS 490 DIP
+% Median Filter
 
+% TODO: add slice size specification, out of bounds detection and discard
 function [r] = Median_Filter(img, slice)
     [row, col] = size(img);
     fimg = zeros(row, col);
+
     for i = 2:row-1
         for j = 2:col-1
             flt=[img(i-1,j-1),img(i-1,j),img(i-1,j+1),img(i,j-1),img(i,j),img(i,j+1),img(i+1,j-1),img(i+1,j),img(i+1,j+1)];
