a4373a62d95d966faa543be7bbceea50ff006018
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-13 19:18:28 -0500

    Update gaussian.m

--- gaussian.m
+++ gaussian.m
@@ -1,1 +1,10 @@
-function guassianFilter = ()
+% Team 255
+% CS 490 DIP
+% Spatial Gaussian Filter
+
+function [g] = gaussian(p, s)
+  % p - input image
+  % s - sigma
+  % g - output image
+  
+end
