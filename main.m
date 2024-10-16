c1d60c62b5a34a6e0834c0d2700aa901307f751c
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-16 12:16:19 -0500

    Other noise types

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ imds = imageDatastore("dataset/*.jpg");
 % Store images from dataset in array
 imgs = readall(imds);
 % To use: change number within curly brackets to desired image number
-a = imgs{11};
+a = imgs{1};
 
 % Standard Deviation affects how noisy the resulting image is
 desiredstd = 32;
@@ -16,20 +16,66 @@ desiredmu = 32;
 % Must be odd otherwise there's no middle pixel to change
 sliceSize = 5;
 
+% Salt and Pepper Noise Density
+dens = 0.08;
+
+% Speckle Noise Variance
+var = 0.08;
+
+
+subplot(3,3,1), imshow(a,[]);
+title("Original Image");
 
 % Add gaussian noise
 [p] = gaussian(a, desiredstd, desiredmu);
 
-subplot(1,3,1), imshow(a,[]);
-title("Original Image");
-
-subplot(1,3,2), imshow(p,[]);
+subplot(3,3,2), imshow(p,[]);
 description = strcat("σ = ", string(desiredstd), ", μ = ", string(desiredmu));
-title({"Noisy Image", description});
+title({"Gaussian Noise", description});
 
 % Apply median filter
-[m] = Median_Filter(p, sliceSize);
+[m1] = Median_Filter(p, sliceSize);
 
-subplot(1,3,3), imshow(m,[]);
+subplot(3,3,3), imshow(m1,[]);
+description = strcat(string(sliceSize), "x", string(sliceSize), " slice");
+title({"Median Filtered", description});
+
+
+
+
+subplot(3,3,4), imshow(a,[]);
+title("Original Image");
+
+% Apply salt and pepper noise
+sp = imnoise(a, "salt & pepper", dens);
+
+subplot(3,3,5), imshow(sp,[]);
+description = strcat("density = ", string(dens));
+title({"S&P Noise", description});
+
+% Apply median filter
+[m2] = Median_Filter(sp, sliceSize);
+
+subplot(3,3,6), imshow(m2,[]);
+description = strcat(string(sliceSize), "x", string(sliceSize), " slice");
+title({"Median Filtered", description});
+
+
+
+
+subplot(3,3,7), imshow(a,[]);
+title("Original Image");
+
+% Apply salt and pepper noise
+speck = imnoise(a, "speckle", var);
+
+subplot(3,3,8), imshow(speck,[]);
+description = strcat("variance = ", string(var));
+title({"Speckle Noise", description});
+
+% Apply median filter
+[m3] = Median_Filter(speck, sliceSize);
+
+subplot(3,3,9), imshow(m3,[]);
 description = strcat(string(sliceSize), "x", string(sliceSize), " slice");
 title({"Median Filtered", description});
